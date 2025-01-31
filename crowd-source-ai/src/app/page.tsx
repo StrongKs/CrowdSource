613cbcdad31dd8e40d3b25299759af84cb709ddf
StringKs <kent.phipps@ufl.edu>
2025-01-31 13:30:05 -0500

    bootstrap Next.js

---
+++ crowd-source-ai/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>Crowd Source AI</h1>
+      <p>Help us make AI better by contributing to our data sets</p>
+    </div>
+  );
+}
