cd945b32cf4c18a02382302b435274211f8d1ac5
StringKs <kent.phipps@ufl.edu>
2025-02-04 02:51:32 -0500

    add default post functionality with red button in homepage

--- crowd-source-ai/src/app/page.tsx
+++ crowd-source-ai/src/app/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+// import Image from "next/image";
+"use client";
+
+import { addDefaultPost } from "@/actions/db.action";
 
 export default function Home() {
+
   return (
-    <div >
+    <div>
       <h1>Crowd Source AI</h1>
       <p>Help us make AI better by contributing to our data sets</p>
+      <button
+        style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer", color: "red", outline: "2px solid blue" }}
+        onClick={() => { 
+          console.log("Button clicked"); 
+          addDefaultPost(); 
+        }}
+      >
+        Add Default Post
+      </button>
     </div>
   );
 }
