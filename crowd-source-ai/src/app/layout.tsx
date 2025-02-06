cb6536ba4f1279ecf60e2da7610794cdabcd8e8a
Jason Ang <jason8417@icloud.com>
2025-02-05 20:10:31 -0500

    navbar and optional navbar files in case we need more than one webpage

--- crowd-source-ai/src/app/layout.tsx
+++ crowd-source-ai/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -35,7 +36,27 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+
+          <div className="min-h-screen">
+              <Navbar />
+
+              <main className="py-8">
+                {/* container to center the content */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">
+                      {/* <Sidebar /> */}
+                      sidebar
+                    </div>
+                    <div className="lg:col-span-9">{children}</div>
+                    
+                  </div>
+                </div>
+              </main>
+            </div>
+
+
+         
           </ThemeProvider>
 
       </body>
--- crowd-source-ai/src/app/page.tsx
+++ crowd-source-ai/src/app/page.tsx
@@ -4,12 +4,10 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
-import ModeToggle from '@/components/ModeToggle'
 
 function HomePage() {
     return (
       <div className="h-screen flex justify-center items-start">
-        <ModeToggle />
         <div className="max-w-sm mt-20">
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Input type="Title" id="Title" placeholder="Type your Title Here" />
