8ed911f849696a85f737de7bac4d6d04817d93a3
Jason Ang <jason8417@icloud.com>
2025-02-07 14:59:25 -0500

    attempt of fix of hydration error

--- crowd-source-ai/src/app/layout.tsx
+++ crowd-source-ai/src/app/layout.tsx
@@ -31,41 +31,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-       <ThemeProvider
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-
-          <div className="min-h-screen">
+            <div className="min-h-screen">
               <Navbar />
 
               <main className="py-8">
                 {/* container to center the content */}
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                  <div className="hidden lg:block lg:col-span-3">
-                      {/* <Sidebar /> */}
+                    <div className="hidden lg:block lg:col-span-3">
                       <Sidebar />
                     </div>
                     <div className="lg:col-span-9">{children}</div>
-                
                   </div>
-                  
                 </div>
               </main>
             </div>
-
             <Toaster />
-         
           </ThemeProvider>
-
-      </body>
-    </html>
+        </body>
+      </html>
   );
 }
